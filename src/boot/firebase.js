6abdb49be3d0c8a2dd14e2c7d010f5a28fa81b48
Mansehej <mansehej@gmail.com>
2020-04-10 02:36:18 +0530

    Make app specific firebase imports

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,14 +1,13 @@
-import * as firebase from "firebase"
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/firestore'
 
 //import firebase config
-const firebaseConfig=require('../../firebaseConfig.json')
+import * as firebaseConfig from "../../firebaseConfig.json";
 
 // Initialize Firebase
-let firebaseApp = firebase.initializeApp(firebaseConfig);
+let firebaseApp = firebase.initializeApp(firebaseConfig.default);
 let firebaseAuth = firebaseApp.auth()
-let   firebaseDb = firebaseApp.firestore();
+let firebaseDb = firebaseApp.firestore();
 
-//Enable analytics
-firebase.analytics();
-
-export {firebaseAuth, firebaseDb, firebase}
+export {firebaseAuth, firebaseDb, firebaseApp}
