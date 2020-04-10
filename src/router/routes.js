551b5fb28f63e999c97b7a1a9d06c522f59ed68d
Aashish Khandelwal <ashishkhandelwal404@gmail.com>
2020-04-11 02:48:27 +0530

    Login Teacher Added

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,25 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
-      { path: '/write', component: () => import('components/Writer.vue') },
-      { path: '/auth', component: () => import('pages/Auth.vue') }
+      { path: "", component: () => import("pages/Index.vue") },
+      { path: "/write", component: () => import("components/Writer.vue") },
+      { path: "/auth", component: () => import("pages/Auth.vue") },
+      {
+        path: "/auth/teacher",
+        component: () => import("pages/AuthTeacher.vue")
+      }
     ]
   }
-]
+];
 
 // Always leave this as last one
-if (process.env.MODE !== 'ssr') {
+if (process.env.MODE !== "ssr") {
   routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
 }
 
-export default routes
+export default routes;
