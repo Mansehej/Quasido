5d005b702aee19961579446e41ee0c4a78d6575a
Mansehej <mansehej@gmail.com>
2020-04-21 22:47:58 +0530

    Change version button to sign out button

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,15 +13,15 @@
 
         <q-toolbar-title>Quasar App</q-toolbar-title>
 
-        <q-btn @click="authChange">Quasar v{{ $q.version }}</q-btn>
+        <q-btn @click="authChange">Sign Out</q-btn>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
-      <q-list>
+      <!-- <q-list>
         <q-item-label header class="text-grey-8">Essential Links</q-item-label>
         <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
-      </q-list>
+      </q-list> -->
     </q-drawer>
 
     <q-page-container>
