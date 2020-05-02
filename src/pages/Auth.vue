5c0ca171a14ea7cbde76abc5e510d2169d292a3f
Mansehej <mansehej@gmail.com>
2020-05-02 18:05:04 +0530

    Remove unnecessary code from aauth

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,12 +1,5 @@
 <template>
   <q-page class="flex flex-center">
-    <!-- <q-img
-      src="../assets/logo.svg"
-      spinner-color="white"
-      class="fixed-bottom-left"
-      style="height:90vh; width:90vh; margin-left:30vh; opacity:0.8"
-    />-->
-
     <q-card style="width:50hh">
       <q-card-section>
         <div class="text-h6 text-capitalize text-center">{{userType}} Login</div>
@@ -28,23 +21,6 @@
       <q-card-section class="q-pa-xs">
         <q-btn flat class="text-capitalize full-width" @click="changeAuth()">Login as {{otherType}}</q-btn>
       </q-card-section>
-      <!-- <div
-        style="margin-top:1vh"
-        class="text-subtitle text-center text-weight-light v"
-        v-if="tab=='login'"
-      >Don't have an account?</div>
-      <div
-        style="margin-top:1vh"
-        class="text-subtitle text-center text-weight-light v"
-        v-if="tab=='register'"
-      >Already have an account?</div>-->
-      <!-- <q-tabs v-model="tab" dense class="bg-white-3" align="justify" narrow-indicator>
-        <q-tab name="login" label="Login" v-if="tab=='register'" />
-        <q-tab name="register" label="Register" v-if="tab=='login'" />
-      </q-tabs>-->
-      <!-- <q-card-section align="justify-around">
-        <google-login/>
-      </q-card-section>-->
     </q-card>
   </q-page>
 </template>
@@ -72,7 +48,6 @@ export default {
   },
   components: {
     "login-register": require("../components/LoginRegister.vue").default
-    // "google-login": require("../components/GoogleLogin.vue").default
   },
   methods: {
     changeAuth() {
